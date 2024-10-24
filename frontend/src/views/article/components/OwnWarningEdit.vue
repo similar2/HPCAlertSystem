7b081719a169e3b410afa55063801d3bf789d3b1
similar2 <shimile18@gmail.com>
2024-10-24 20:56:34 +0800

    update custom alerting rule logic

--- frontend/src/views/article/components/OwnWarningEdit.vue
+++ frontend/src/views/article/components/OwnWarningEdit.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 import { artAddRulesService } from '@/api/Warning'
-import MetricNameSelect from './MetricNamesSelect.vue'
-import FilterSelect from './FilterSelect.vue'
+// import MetricNameSelect from './MetricNamesSelect.vue'
+// import FilterSelect from './FilterSelect.vue'
 import { ElMessage } from 'element-plus'
 // import { baseURL } from '@/utils/request'
 // import axios from 'axios'
@@ -10,7 +10,7 @@ const visibleDrawer = ref(false)
 const emit = defineEmits(['success'])
 
 const defaultForm = {
-  type: 'BMC',
+  type: '',
   alertName: '',
   metricName: '',
   expr: '',
@@ -19,19 +19,20 @@ const defaultForm = {
   description: '',
   filters: ''
 }
-const validateExpr = (rule, value, callback) => {
-  const pattern = /^(>|<|=)\s\d+$/
-  if (!pattern.test(value)) {
-    callback(new Error('Invalid format. It should be like "> 40"'))
-  } else {
-    callback()
-  }
-}
+// const validateExpr = (rule, value, callback) => {
+//   const pattern = /^([><=])\s\d+$/
+//   if (!pattern.test(value)) {
+//     callback(new Error('Invalid format. It should be like "> 40"'))
+//   } else {
+//     callback()
+//   }
+// }
 const rules = {
   type: [{ required: true, message: '请输入type', trigger: 'blur' }],
   alertName: [{ required: true }],
   metricName: [{ required: true }],
-  expr: [{ required: true }, { validator: validateExpr, trigger: 'blur' }],
+  // expr: [{ required: true }, { validator: validateExpr, trigger: 'blur' }],
+  expr: [{ required: true }, { trigger: 'blur' }],
   severity: [{ required: true }],
   description: [{ required: true }],
   filters: [{ required: true }]
@@ -66,7 +67,7 @@ defineExpose({
 <template>
   <el-drawer
     v-model="visibleDrawer"
-    :title="'添加错误'"
+    :title="'添加报警规则'"
     direction="rtl"
     size="50%"
   >
@@ -78,11 +79,18 @@ defineExpose({
       label-width="100px"
     >
       <el-form-item label="硬件类型" prop="type">
-        <el-input
-          v-model="formModel.type"
-          placeholder="请输入硬件类型"
-        ></el-input>
+        <el-select v-model="formModel.type" placeholder="请输入硬件类型">
+          <el-option label="BMC" value="BMC"></el-option>
+          <el-option label="HOST" value="HOST"></el-option>
+
+        </el-select>
       </el-form-item>
+      <!--      <el-form-item label="硬件类型" prop="type">-->
+      <!--        <el-input-->
+      <!--          v-model="formModel.type"-->
+      <!--          placeholder="请输入硬件类型"-->
+      <!--        ></el-input>-->
+      <!--      </el-form-item>-->
 
       <el-form-item label="告警名" prop="alertName">
         <el-input
@@ -90,13 +98,13 @@ defineExpose({
           placeholder="请输入告警名"
         ></el-input>
       </el-form-item>
-      <el-form-item label="metric" prop="metricName">
-        <MetricNameSelect
-          v-model="formModel.metricName"
-          :type="formModel.type"
-          width="100%"
-        ></MetricNameSelect>
-      </el-form-item>
+      <!--      <el-form-item label="metric" prop="metricName">-->
+      <!--        <MetricNameSelect-->
+      <!--          v-model="formModel.metricName"-->
+      <!--          :type="formModel.type"-->
+      <!--          width="100%"-->
+      <!--        ></MetricNameSelect>-->
+      <!--      </el-form-item>-->
       <el-form-item label="表达式" prop="expr">
         <el-input
           v-model="formModel.expr"
@@ -130,14 +138,14 @@ defineExpose({
         ></el-input>
       </el-form-item>
 
-      <el-form-item label="过滤器" prop="filters">
-        <FilterSelect
-          v-model="formModel.filters"
-          :type="formModel.type"
-          :metric="formModel.metricName"
-          width="100%"
-        ></FilterSelect>
-      </el-form-item>
+      <!--      <el-form-item label="过滤器" prop="filters">-->
+      <!--        <FilterSelect-->
+      <!--          v-model="formModel.filters"-->
+      <!--          :type="formModel.type"-->
+      <!--          :metric="formModel.metricName"-->
+      <!--          width="100%"-->
+      <!--        ></FilterSelect>-->
+      <!--      </el-form-item>-->
       <el-form-item>
         <el-button @click="onPublish()" typeName="primary">添加</el-button>
       </el-form-item>
@@ -152,6 +160,7 @@ defineExpose({
       height: 178px;
       display: block;
     }
+
     .el-upload {
       border: 1px dashed var(--el-border-color);
       border-radius: 6px;
@@ -160,9 +169,11 @@ defineExpose({
       overflow: hidden;
       transition: var(--el-transition-duration-fast);
     }
+
     .el-upload:hover {
       border-color: var(--el-color-primary);
     }
+
     .el-icon.avatar-uploader-icon {
       font-size: 28px;
       color: #8c939d;
