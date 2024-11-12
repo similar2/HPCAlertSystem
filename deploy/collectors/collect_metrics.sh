bc5baa9685ccf2583bbe49e8926cc02c258d3949
similar2 <shimile18@gmail.com>
2024-11-12 17:45:55 +0800

    更新用户统计脚本

--- deploy/collectors/collect_metrics.sh
+++ deploy/collectors/collect_metrics.sh
@@ -9,19 +9,21 @@ mkdir -p "$METRICS_DIR"
 
 # Function to collect logged-in users
 collect_logged_in_users() {
-    # Count logged-in users
-    user_count=$(who | wc -l)
-
-    touch "$METRICS_DIR/logged_in_users.prom"
+    # Get unique logged-in users
+    unique_user_count=$(who | awk '{print $1}' | sort | uniq | wc -l)
 
     # Write metric to the .prom file in Prometheus format
-    echo "# HELP logged_in_users Number of logged-in users." > "$METRICS_DIR/logged_in_users.prom"
-    echo "# TYPE logged_in_users gauge" >> "$METRICS_DIR/logged_in_users.prom"
-    echo "logged_in_users $user_count" >> "$METRICS_DIR/logged_in_users.prom"
+    metrics_file="$METRICS_DIR/logged_in_users.prom"
+    touch "$metrics_file"
+
+    # Overwrite with new data in Prometheus format
+    echo "# HELP logged_in_users Number of unique logged-in users." > "$metrics_file"
+    echo "# TYPE logged_in_users gauge" >> "$metrics_file"
+    echo "logged_in_users $unique_user_count" >> "$metrics_file"
 }
 
-# Main loop - this will run indefinitely, updating the metrics every 1 seconds (adjust as needed)
+# Main loop - runs indefinitely, updating the metrics every 1 second (adjust as needed)
 while true; do
     collect_logged_in_users
-    sleep 1  # Sleep for 1 seconds before updating again
+    sleep 1  # Sleep for 1 second before updating again
 done
