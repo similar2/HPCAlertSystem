6c19836e26ff752e2cb50f197836d847d30c5a2c
similar2 <shimile18@gmail.com>
2024-12-21 17:31:55 +0800

    jobname匹配规则添加

--- backend/hpc-monitor-web/src/main/java/edu/sustech/hpc/service/JobMappingService.java
+++ backend/hpc-monitor-web/src/main/java/edu/sustech/hpc/service/JobMappingService.java
@@ -1,14 +1,8 @@
 package edu.sustech.hpc.service;
 
-import edu.sustech.hpc.model.vo.HardwareInfo;
-import edu.sustech.hpc.model.vo.PrometheusTargetInfo;
-import edu.sustech.hpc.po.Hardware;
 import edu.sustech.hpc.po.HardwareType;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class JobMappingService {
@@ -21,14 +15,13 @@ public class JobMappingService {
         return switch (hardwareType) {
             case BMC -> bmcService;
             case HOST -> hostService;
-            default -> null;
         };
     }
 
     public JobService getJobService(String jobName) {
         return switch (jobName) {
-            case "ipmi_exporter" -> bmcService;
-            case "node_exporter" -> hostService;
+            case "ipmi_exporter", "ipmi" -> bmcService;
+            case "node_exporter", "node" -> hostService;
             default -> null;
         };
     }
@@ -37,12 +30,4 @@ public class JobMappingService {
         return getJobService(jobName)._getHardwareType();
     }
 
-    public HardwareInfo getHardwareInfo(PrometheusTargetInfo prometheusTargetInfo) {
-        return new HardwareInfo(getHardwarePo(prometheusTargetInfo));
-    }
-
-    public Hardware getHardwarePo(PrometheusTargetInfo prometheusTargetInfo) {
-        return getJobService(prometheusTargetInfo.getJobName())
-                ._getHardwarePo(prometheusTargetInfo);
-    }
 }
